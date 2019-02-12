1ef851960b5d80996feb8470eddd0a30fcab4566
alphons666 <39222757+alphons666@users.noreply.github.com>
2019-02-11 21:10:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+const bodyparser = require('body-parser')
+const validUrl = require('valid-url');
+
+const UrlFunc = require('./models/UrlModel')
+
 var cors = require('cors');
 
 var app = express();
@@ -12,9 +17,11 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
 
 app.use(cors());
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json())
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
@@ -25,7 +32,37 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post('/api/shorturl/new', async (req, res) => {
+  console.log(`${req.path} ${JSON.stringify(req.body)}`)
+  let { url } = req.body
+  try {
+    if(!validUrl.isWebUri(url))
+      return res.json({ error: "invalid URL" })
+
+    let shorted = await UrlFunc.shortThis(url)
+    console.log(JSON.stringify(shorted))
+    res.json({original_url: shorted.url, short_url: shorted.id, shorted})
+    
+  } catch (ex) {
+    console.error(ex)
+  }
+})
+
+app.get('/api/shorturl/:id?', async (req, res) => {
+  let { id } = req.params, obj
   
+  if(!!!id) return res.redirect('/')
+  try{
+    obj = await UrlFunc[/^\d+$/.test(id)? 'getById':'getByAlias'](id)
+    obj = obj || {}
+    res.redirect(301, obj.url || '/')
+  } catch (ex) {
+    console.error(ex)
+    res.redirect('/')
+  }
+})
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
